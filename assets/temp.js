02a11a60dfd4d554bb0f005e0bfc58f67808f8d9
nathan.latino <nathan.latino@he-arc.ch>
2019-03-06 14:23:11 +0100

    modification du fichier temp.js

--- assets/temp.js
+++ assets/temp.js
@@ -30,11 +30,11 @@ function getSigne(value) {
 	return 0 < value ? 0 : 1;
 }
 
-function toBinaireInteger(value) {
+function toBinaireInteger(value, nbBit) {
 	let tabBin = [];
 	let valueFloat = value;
-	let count = 15;
-	for (var i = 0; i < count; i++) {
+	let count = nbBit;
+	for (let i = 0; i < count; i++) {
 		tabBin.push(valueFloat % 2);
 		valueFloat = parseInt(valueFloat / 2);
 	}
@@ -43,13 +43,18 @@ function toBinaireInteger(value) {
 	return tabBin;
 }
 
-function toBinaireDecimal(value) {
+function cleanTab(binTab) {
+
+}
+
+function toBinaireDecimal(value, nbBit) {
 	let tabBin = [];
 	let valueFloat = value
 	//taille Mantisse
-	let count = 15;
+	let count = nbBit;
+	console.log(nbBit);
 
-	for (var i = 0; i < count; i++) {
+	for (let i = 0; i < count; i++) {
 		valueFloat = valueFloat * 2;
 		if (valueFloat < 1) {
 			tabBin.push(0);
@@ -62,34 +67,66 @@ function toBinaireDecimal(value) {
 	return tabBin;
 }
 
-function getExponant(tabBin, posComas) {
+function getExponant(tabBin, posComma) {
 	let i = 0;
 	while (i < tabBin.length && tabBin[i] != 1) {
 		i++;
 	}
 
-	return (127 + (posComas - (i + 1))); //taille int +1 (virgule après) + bit caché
+	return (127 + (posComma - (i + 1))); //taille int +1 (virgule après) + bit caché
 }
 
-function toBinaire(value) {
-	//var power = getPower(value);
-	console.log(value % 1);
-	console.log(parseInt(value));
+function cleanTab(tabBinInt) {
+	tabBin = [];
+	let i = 0;
+	let flag = 0;
+	while (i < tabBinInt.length){
+		if(flag == 1){
+			tabBin.push(tabBinInt[i]);
+		}
+		if (tabBinInt[i] == 1) {
+			flag = 1;
+		}
+		i++;
+	}
+
+	return tabBin;
+}
+
+function AddBinaire(tabBin1, tabBin2){
+
+
+}
+
+function toBinaire(value, nbBit) {
+	
+	
 	let tabBinInt = [];
 	let tabBinFloat = [];
 	let exponent;
 
 	//tabBin
-	tabBinInt = toBinaireInteger(parseInt(value));
-	tabBinFloat = tabBinInt.concat(toBinaireDecimal(value % 1));
+	console.log(parseInt(value));
+	tabBinInt = toBinaireInteger(parseInt(value),nbBit-9); // juste mantisse
+	tabBinInt = cleanTab(tabBinInt);
+	//ajout de la précision avec bit
+	console.log(value % 1);
+	tabBinFloat = tabBinInt.concat(toBinaireDecimal(value % 1,nbBit-9-tabBinInt.length)); 
+	tabBinFloat = cleanTab(tabBinFloat);
 
 	print(tabBinFloat);
 
 	//Exponent
-	exponent = getExponant(tabBinFloat, 15);
+	//getExponant(tabBinFloat, tabBinInt.length);
+	exponent = 127 + tabBinInt.length;
 	console.log(exponent);
-	tabBinExp = toBinaireInteger(exponent);
-	tabBinExp = toBinaireInteger(exponent);
+	console.log(tabBinInt.length);
+	tabBinExp = toBinaireInteger(exponent, 8); // 8 bit exposant
+
+	let tabBin = [];
+	tabBin.push(getSigne(value));
+	tabBin = tabBin.concat(tabBinExp.concat(tabBinFloat));
+	print(tabBin);
 }
 
 function print(tabBin) {
@@ -101,10 +138,10 @@ function print(tabBin) {
 }
 
 
-var input = 263.3;
+var input = 32.4;
 
 console.log(input);
-var tabBin = toBinaire(input);
+var tabBin = toBinaire(input, 32);
 //print(tabBin);
 
 //2*r * PI = Perimetre
@@ -113,3 +150,24 @@ var tabBin = toBinaire(input);
 //Si Diam = 1 -> Perimetre = PI
 //r = Diam/2
 //
+/* TEST
+263.3
+0 10000111 00000111010011001100110
+0 10000111 00000111010011001100110
+
+0.3
+0 01111101 00110011001100110011010
+0 01111111 01001100110011001100110
+
+4
+0 10000001 00000000000000000000000
+0 10000001 00000000000000000000000
+
+32
+0 10000100 00000000000000000000000
+0 10000100 00000000000000000000000
+
+32.4
+0 10000100 10011001100110010000000
+0 10000100 10011001100110010000000
+*/
