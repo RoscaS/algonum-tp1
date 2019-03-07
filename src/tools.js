bf99cd301ef0afaf5770b3408aba72dc59e7b448
roscas <sol.rosca@he-arc.ch>
2019-03-07 18:49:40 +0100

    rewrite + tests sets

--- src/tools.js
+++ src/tools.js
@@ -1,8 +1,9 @@
-const EXPONENT = 8;
-const MANTISSA = 23;
 
-function range(start, end) {
-  return [...Array(end).keys()].map(i => i + start);
+function split(value) {
+  let sign = parseSign(value);
+  let splitted = value.replace('-', '').split('.');
+  splitted.push('0'); // in case of integer value
+  return {integer: splitted[0], fraction: splitted[1], sign: sign};
 }
 
 function parseSign(value) {
@@ -10,16 +11,26 @@ function parseSign(value) {
 }
 
 function intToBin(value) {
-  return value ? (value % 2) + intToBin(Math.floor(value / 2)) * 10 : 0;
+  let result = value ? (value % 2) + intToBin(Math.floor(value / 2)) * 10 : 0;
+  return result.toString();
 }
 
-function fractionToBin(value) {
+function leadingZeros(value) {
+  return `${range(0, EXPONENT - value.length, '0').join('')}${value}`;
+}
+
+function fractionToBin(value, size) {
   let bin = '';
   value /= (Math.pow(10, value.toString().length));
-  range(0, MANTISSA).forEach(() => {
+
+  range(0, size).forEach(() => {
     let v = value - parseInt(value);
     bin += (v * 2) > 1 ? '1' : '0';
     value *= 2;
   });
   return bin;
+}
+
+function bias(exponentBits) {
+  return (RANGE['32'].upper + exponentBits).toString();
 }
